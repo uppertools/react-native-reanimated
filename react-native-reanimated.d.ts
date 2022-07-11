8b81eb08fda96ded4eab50fed0250f8482fba3e9
Angelika Serwa <angelika.serwa@swmansion.com>
2022-07-11 11:35:38 +0200

    Ts

--- react-native-reanimated.d.ts
+++ react-native-reanimated.d.ts
@@ -637,12 +637,12 @@ declare module 'react-native-reanimated' {
     velocity?: number;
   }
   export function withTiming<T extends AnimatableValue>(
-    toValue: T,
+    toValue: T | SharedValue<T>,
     userConfig?: WithTimingConfig,
     callback?: AnimationCallback
   ): T;
   export function withSpring<T extends AnimatableValue>(
-    toValue: T,
+    toValue: T | SharedValue<T>,
     userConfig?: WithSpringConfig,
     callback?: AnimationCallback
   ): T;
@@ -653,7 +653,7 @@ declare module 'react-native-reanimated' {
   export function cancelAnimation<T>(sharedValue: SharedValue<T>): void;
   export function withDelay<T extends AnimatableValue>(
     delayMS: number,
-    delayedAnimation: T
+    delayedAnimation: T | SharedValue<T>
   ): T;
   export function withRepeat<T extends AnimatableValue>(
     animation: T,
